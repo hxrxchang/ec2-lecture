eaa69db72efb6028470a35ee8e0728d7a100efaf
hxrxchang <m.y.y.0326.cg@gmail.com>
2018-05-21 16:57:31 +0900

    modified

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'hogehogehogehoge' });
+  res.render('index', { title: 'express', server: 'ec2' });
 });
 
 module.exports = router;
